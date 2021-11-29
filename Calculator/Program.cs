008aa098715d1182f4f3e5cd5257aa443b481966
Александр Ерхов <tyrant321@yandex.ru>
2021-11-29 23:40:16 +0300

    One more project

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,40 +1,8 @@
 ﻿using System;
+using CalculatorLibrary;
 
-namespace Calculator
+namespace CalculatorProgram
 {
-    class Calculator
-    {
-        public static double DoOperation(double num1, double num2, string op)
-        {
-            double result = double.NaN; // Default value is "not-a-number" if and operation, such as division, could result in an error.
-
-            // Use a switch statement to do the math.
-            switch (op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    break;
-                case "m":
-                    result = num1 * num2;
-                    break;
-                case "d":
-                    // ask the user to enter a non-zero division.
-                    if (num2 != 0)
-                    { 
-                        result = num1 / num2;
-                    }
-                    break;
-                //return text for an incorrect option entry.
-                default:
-                    break;
-            }
-            return result;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if and operation, such as division, could result in an error.
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    // ask the user to enter a non-zero division.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                //return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
